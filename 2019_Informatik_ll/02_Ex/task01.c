6ae4de9cf0d80d4795345d4790c072a42cadbef3
olstra <seriously251@gmail.com>
2019-02-26 16:43:01 +0100

    Auto stash before merge of "master" and "origin/master"

---
+++ 2019_Informatik_ll/02_Ex/task01.c
@@ -0,0 +1,33 @@
+/**
+ *	Task: 01
+ *	Exersize: 02
+ *	Informatik II, UZH, FS19
+ *	Author: Oliver Strassmann
+ *
+ * */
+
+#include <stdio.h>
+
+
+int LCM( int x, int y, int result ) {
+
+
+}
+
+int GCD( int x, int y ) {
+
+	if ( x == y ) { return x; }
+	
+	if( x > y ) { return GCD( x-y, y ); }
+}
+
+
+
+
+int main() {
+
+
+
+
+
+}
