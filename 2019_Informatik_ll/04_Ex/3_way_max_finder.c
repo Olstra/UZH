ef1ab4cf4a731732bb4be28198ef8a228a6409e7
olstra <seriously251@gmail.com>
2019-03-26 13:41:39 +0100

    updates

--- 2019_Informatik_ll/04_Ex/3_way_max_finder.c
+++ 2019_Informatik_ll/04_Ex/3_way_max_finder.c
@@ -12,28 +12,37 @@
 #include <stdio.h>
 
 
+int findMaxOf3( int a, int b, int c ) {
+
+	if ( a >= b && a >= c ) { return a; }
+	else if ( b > a && b>= c ) { return b; }
+	else return c;
+
+}
+
+
+int count = 1;
 int maxFinder( int A[], int l, int r ) {
 
 	// special cases
 
-	if( r == 1 ) return A[0];	// array contains only 1 e
-	
+	if( r-l == 1 ) return A[l];	// array contains only 1 e
+printf( "\n %d-Max: %d\n", count, A[l] );
+	count++;
 	//if( r == 2 ) return ( A[0] > A[1] ) ? A[0] : A[1]; // array contains only 2 e 
-
+	//if ( r-l <= 1 ) return A[l];
 	// recursive call untill partitions of 1
 
-	int mid1 = r/3;		// = 1/3
-	int mid2 = mid1 * 2;	// = 2/3
+	int mid1 = ( l + r ) / 3; // = 1/3
+	int mid2 = ( ( l+ r) / 3 ) * 2;	 // = 2/3
+	printf( "\nmid1: %d mid2: %d\n", mid1, mid2 );
 	
 	int a = maxFinder( A, l, mid1-1 );	
 	int b = maxFinder( A, mid1, mid2-1 );		
 	int c = maxFinder( A, mid2, r-1 ); 
 
 	// return maximum out of 3 nums	
-	if ( a >= b && a >= c ) return a;
-	else if ( b > a && b >= c ) return b;
-	else return c;
-
+	return findMaxOf3( a, b, c );
 }
 
 
@@ -43,7 +52,7 @@ int main() {
 
 	int A[] = { 2, 90 };
 	
-	int B[] = { 5, 90, 8 };
+	int B[] = { 90, 5, 70, 8 };
 
 	int C[] = { 90 };
 
@@ -53,15 +62,11 @@ int main() {
 
 	//int testNums[] = { A, B, C, D, E };
 
-	//int testLens[] = { 2, 3, 1, 11, 3 };
+	int testLens[] = { 2, 3, 1, 11, 3 };
 
 	int nrOfTests = 5;
 
-	//for( int i = 0; i < nrOfTests; i++ ) {
-	//	printf( "\nTest %d/%d - Max: %d\n", i+1, nrOfTests, maxFinder( testNums[i], 0, testLens[i] ) );
-	//}
-
-	printf ( "\nMax: %d\n", maxFinder( D, 0, 11 ) );
+	printf ( "\nMax: %d\n", maxFinder( B, 0, 3 ) );
 
 
 	return 0;
