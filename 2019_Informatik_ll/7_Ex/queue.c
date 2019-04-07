e05282cc4575cc58d0d5552aee030f31cb2da1e9
olstra <seriously251@gmail.com>
2019-04-07 14:01:59 +0200

    z

---
+++ 2019_Informatik_ll/7_Ex/queue.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void initialize(queue *q){}
+
+int enqueue(queue *q, int value){}
+
+int dequeue(queue *q){}
+
+int compareQueue(queue *qA, queue *qB){}
+
+
+int main(){
+
+	initialize(stack);
+
+	return 0;
+}
