e16447440fbe29a33bba07b6a7e4bb50cff8d05c
olstra <seriously251@gmail.com>
2019-04-07 20:59:45 +0200

    r

--- 2019_Informatik_ll/7_Ex/stack.c
+++ 2019_Informatik_ll/7_Ex/stack.c
@@ -1,37 +1,99 @@
 #include <stdio.h>
+#include <stdlib.h>
 
+const int INITIAL_STACK_SIZE = 5;	
 
-int INITIAL_STACK_SIZE = 5;	
+struct stack{
+	const int* root;		// points to "struct stackArray[0]"
+	int size;		// keep track of total size of struct stack
+	int topElement; // save index of element at top of struct stack (LIFO) 
+};
 
-typedef struct stackADT {
-	int* elements;
-	int size;
-	int count;
-} stack;
-
-void initialize(stack *s){
-		
-	stack->size = INITIAL_SZACK_SIZE;
+void initialize(struct stack *s){
+	
  	// create array to save elements in
-	static int elements[INITIAL_STACK_SIZE];
-	stack->elements = elements; // point elements pointer to elements array
-	int count = 0;
+	const int elements[INITIAL_STACK_SIZE];
+
+	// set values of stack struct	
+	s->size = INITIAL_STACK_SIZE; 
+	s->root = elements[0]; // point root to array
+	s->topElement = 0;	// [0] element remains empty as root
 }
 
-int isEmpty(stack *s){}
+int isEmpty(struct stack *s){
 
-int push(stack *s, int value){}
+	if(s->topElement <= 0) return 1; // "TRUE"
 
-int pop(stack *s){}
+	return 0; // "FALSE" else stack is not empty
 
-int compareStack(stack *sA, stack *sB){}
+}
 
-int printStack(stack *s){}
+int push(struct stack *s, int value){
+	
+	s->topElement++; // go to next index to save new element
+
+	// check if we've run out of space
+	if(s->topElement >= s->size){
+		// make stack bigger
+		s->size += INITIAL_STACK_SIZE;
+		const int newArray[s->size];
+		s->root = newArray[0];
+
+		// copy elements to new array
+		for(int i = 1; i < s->size; i++){
+			newArray[i] = s->root[i];
+		} 
+	}
+	
+	// add new element
+	newArray[s->topElement] = val;
+
+	return s->topElement;
+
+}
+
+int pop(struct stack *s){
+	
+	// check if struct stack is empty
+	if(isEmpty(s) == 1) return -1;
+
+	int deletedVal = s->root[s->topElement];
+
+	s->topElement--; // reduce index of "last element"	
+	
+	return deletedVal;	
+
+}
+
+int compareStack(struct stack *sA, struct stack *sB){
+	
+	// if stacks have different sizes they're not equal
+	if(sA->topElement != sB->topElement) return 0;
+
+	for(int i = 1; i < sA->topElement; i++){
+		if(sA->root[i] != sB->root[i]) return 0;
+	}
+	
+	return 1; // sA and sB are equal
+}
+
+void printStack(struct stack *s){
+
+	int temp = 1;
+
+	printf("\nFrom the top of the stack:");
+	while(temp <= s->topElement){
+		printf(" %d", s->root[temp]);
+		temp++;	
+	}
+
+}
 
 
 int main(){
-
-	// initialize(stack);
+	struct stack* myStack = (struct stack*) malloc( sizeof(struct stack));
+	
+	initialize(myStack);
 
 	return 0;
 }
