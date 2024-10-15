456d1d36954bbeaeb839412758d98c164d1097f0
olstra <seriously251@gmail.com>
2024-10-15 11:39:26 +0200

    Add Web & Mobile Acc. repo

---
+++ 2023_Web_and_Mobile_Accessibility/README.md
@@ -0,0 +1,19 @@
+# 19_Web-Mobile-Accessibility
+Group Project for the course "Web&amp;Mobile Accessibility" at UZH. 
+
+Members of this group are:
+- Oliver Strassmann, 15-932-726
+- Alexander Christian Lerch, 17-678-111
+- Maximilian Achakri, 18-700-385
+- Kirthan Gengatharan, 18-700-476
+
+
+# Run Project
+Download all the applications in the /installable folder --> (gitignored)
+
+Navigate to the /src folder & run the below stated command:
+
+python -m http.server 8000
+
+Then open any browser and enter the following link:
+http://localhost:8000/index.html
---
+++ 2023_Web_and_Mobile_Accessibility/Repo/src/js/login.js
@@ -0,0 +1,120 @@
+/**
+ * Set the form control element to valid
+ * @param {object} element - The DOM element
+ */
+function setValid(element) {
+    element.classList.remove('is-invalid');
+    element.classList.add('is-valid');
+}
+
+/**
+ * Set the form control element to invalid with the error message
+ * @param {object} element - The DOM element
+ */
+function setInvalid(element) {
+    element.classList.remove('is-valid');
+    element.classList.add('is-invalid');
+}
+
+/**
+ * Remove validation information from the element
+ * @param {object} element - The DOM element
+ */
+function removeValidation(element) {
+    element.classList.remove('is-valid');
+    element.classList.remove('is-invalid');
+}
+
+/**
+ * Validate the login form and try to log the user in
+ * @param {object} event - The DOM event
+ */
+function login(event) {
+    event.preventDefault();
+    event.stopPropagation();
+
+    var hasError = false;
+
+    var email = document.getElementById('login-email-control');
+    if (email.validity.valid) {
+        setValid(email);
+    } else if (email.validity.valueMissing) {
+        setInvalid(email);
+        hasError = true;
+    } else {
+        setInvalid(email);
+        hasError = true;
+    }
+
+    var password = document.getElementById('login-password-control');
+    if (password.value.trim().length == 0) {
+        setInvalid(password);
+        hasError = true;
+    } else {
+        setValid(password);
+    }
+
+    if (hasError) {
+        document.getElementById('login-error').classList.remove('d-none');
+    } else {
+        document.getElementById('login-error').classList.add('d-none');
+    }
+}
+
+/**
+ * Validate the login form and try to retrieve the password
+ * @param {object} event - The DOM event
+ */
+function forgot(event) {
+    event.preventDefault();
+    event.stopPropagation();
+
+    var hasError = false;
+
+    var email = document.getElementById('login-email-control');
+    if (email.validity.valid) {
+        setValid(email);
+    } else if (email.validity.valueMissing) {
+        setInvalid(email);
+        hasError = true;
+    } else {
+        setInvalid(email);
+        hasError = true;
+    }
+
+    var password = document.getElementById('login-password-control');
+    removeValidation(password);
+
+    if (hasError) {
+        document.getElementById('login-error').classList.remove('d-none');
+    } else {
+        document.getElementById('login-error').classList.add('d-none');
+    }
+}
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    document
+        .getElementById('login-login-button')
+        .addEventListener('click', login, false);
+
+    document
+        .getElementById('login-forgot-button')
+        .addEventListener('click', forgot, false);
+
+}, false);
+
+
+
+function showTypedInPassword() {
+    var typpedPw = document.getElementById("login-password-control");
+    console.log(typpedPw);
+    console.log(typpedPw.type);
+
+    if (typpedPw.type === "password") {
+        typpedPw.type = "text";
+    } else {
+        typpedPw.type ="password";
+    }
+    
+  }
